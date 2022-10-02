9c108b67c055c2ab6ec1b35eea74c4dad9a206a1
rupor-github <rupor0mail@gmail.com>
2022-10-02 09:30:15 -0400

    Cleaning up build

--- cmake/sign.sh
+++ cmake/sign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # gopass show -o build/minisign
-echo "sysda123" | minisign -S -s ${HOME}/.minisign/build.key -c "gclpr for ${1} release signature" -m gclpr_${1}.zip
+passage show build | minisign -S -s ${HOME}/.minisign/build.key -c "gclpr for ${1} release signature" -m gclpr_${1}.zip
 if [[ ${1} == win* ]]; then
    sed -i -e "s/__CURRENT_HASH_${1}__/$(sha256sum -z gclpr_${1}.zip | awk '{ print $1; }')/g" gclpr.json
 fi
